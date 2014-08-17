cce7c4a8eeee2db6b2efcbaef9501a6cb22000d7
marciosn <marciosouzanobre@gmail.com>
2014-08-16 21:05:23 -0300

    mudança na listagem do json, ainda nao funcionando

--- FichaSuja2014/src/com/app/qx/fichasuja/eleicoes2014/MainActivity.java
+++ FichaSuja2014/src/com/app/qx/fichasuja/eleicoes2014/MainActivity.java
@@ -25,14 +25,14 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 
 @SuppressLint("NewApi")
-public class MainActivity extends Activity implements OnItemClickListener{
+public class MainActivity extends Activity implements OnItemClickListener {
 	private static final String TAG = MainActivity.class.getSimpleName();
-	private String url = "http://api.tcm.ce.gov.br/tre/1_0/processos_gestores.json";
+	private static final String url = "http://api.tcm.ce.gov.br/tre/1_0/processos_gestores.json";
 	private ProgressDialog dialog;
 	private List<Politico> politicos = new ArrayList<Politico>();
 	private ListView listView;
 	private Adapter adapter;
-	
+
 	private static final String GESTOR = "gestor";
 	private static final String PROCESSO = "processo";
 	private static final String MUNICIPIO = "municipio";
@@ -41,6 +41,8 @@ public class MainActivity extends Activity implements OnItemClickListener{
 	private static final String NOTA_IMPROBIDADE = "nota_improbidade";
 	private static final String CODIG0_GESTOR = "codigo_gestor";
 	private static final String CODIGO_MUNICIPIO = "codigo_municipio";
+	private static final String CONTENT = "_content";
+	private static final String CONTENT2 = "rsp";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -55,37 +57,43 @@ public class MainActivity extends Activity implements OnItemClickListener{
 		dialog.setMessage("Carregando");
 		dialog.show();
 		
-		getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1b1b1b")));
+		//getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1b1b1b")));
 		
 		JsonArrayRequest politicoReq = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
 
 			@Override
 			public void onResponse(JSONArray response) {
 				Log.d(TAG, response.toString());
+				Log.i(TAG, response.toString());
 				hideDialog();
-				for(int i = 0; i < response.length(); i++ ){
+				for(int i = 0; i < response.length(); i++ ){	
 					try {
 						JSONObject obj = response.getJSONObject(i);
 						Politico politico = new Politico();
 						
-							politico.setGestor(obj.getString(GESTOR));
-							double processo = Double.parseDouble(obj.getString(PROCESSO));
-							politico.setProcesso(processo);
-							politico.setMunicipio(obj.getString(MUNICIPIO));
-							politico.setNatureza_processo(obj.getString(NATUREZA_PROCESSO));
-							double exercicio = Double.parseDouble(obj.getString(EXERCICIO));
-							politico.setExercicio(exercicio);
-							politico.setNota_improbidade(obj.getString(NOTA_IMPROBIDADE));
-							double codigo_gestor = Double.parseDouble(obj.getString(CODIG0_GESTOR));
-							politico.setCodigo_gestor(codigo_gestor);
-							double codigo_municipio = Double.parseDouble(obj.getString(CODIGO_MUNICIPIO));
-							politico.setCodigo_municipio(codigo_municipio);
-						
-						
+						JSONArray array = obj.getJSONArray(CONTENT);
+							for(int j=0; j < array.length();j++){
+								JSONObject politicoJSON = new JSONObject(array.getString(j));
+								
+								politico.setGestor(politicoJSON.getString(GESTOR));
+								double processo = Double.parseDouble(politicoJSON.getString(PROCESSO));
+								politico.setProcesso(processo);
+								politico.setMunicipio(politicoJSON.getString(MUNICIPIO));
+								politico.setNatureza_processo(politicoJSON.getString(NATUREZA_PROCESSO));
+								double exercicio = Double.parseDouble(politicoJSON.getString(EXERCICIO));
+								politico.setExercicio(exercicio);
+								politico.setNota_improbidade(politicoJSON.getString(NOTA_IMPROBIDADE));
+								double codigo_gestor = Double.parseDouble(politicoJSON.getString(CODIG0_GESTOR));
+								politico.setCodigo_gestor(codigo_gestor);
+								double codigo_municipio = Double.parseDouble(politicoJSON.getString(CODIGO_MUNICIPIO));
+								politico.setCodigo_municipio(codigo_municipio);
+
+							}
+							
 						politicos.add(politico);
 						listView.setAdapter(adapter);
 				        listView.setCacheColorHint(Color.TRANSPARENT);
-				        Toast.makeText(MainActivity.this, politicos.get(0).getGestor(), Toast.LENGTH_LONG).show();
+				        //Toast.makeText(MainActivity.this, politicos.get(0).getGestor(), Toast.LENGTH_LONG).show();
 						
 						
 					} catch (Exception e) {
@@ -106,16 +114,18 @@ public class MainActivity extends Activity implements OnItemClickListener{
 		}); 
 		AppController.getInstance().addToRequestQueue(politicoReq);
 	}
+
 	private void hideDialog() {
 		if (dialog != null) {
 			dialog.dismiss();
 			dialog = null;
 		}
 	}
+
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
