9655fd4d46683a4c2e76b38f946beda83cd3e73e
marciosn <marciosouzanobre@gmail.com>
2014-08-17 14:58:15 -0300

    Json ja é buscado e exibido na view mais ainda de forma errada

--- FichaSuja2014/src/com/app/qx/fichasuja/eleicoes2014/TestaVolley.java
+++ FichaSuja2014/src/com/app/qx/fichasuja/eleicoes2014/TestaVolley.java
@@ -6,34 +6,29 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
-@SuppressLint("NewApi")
-public class MainActivity extends Activity implements OnItemClickListener {
+public class TestaVolley extends Activity {
+
 	private static final String TAG = MainActivity.class.getSimpleName();
 	private static final String url = "http://api.tcm.ce.gov.br/tre/1_0/processos_gestores.json";
 	private ProgressDialog dialog;
 	private List<Politico> politicos = new ArrayList<Politico>();
 	private ListView listView;
 	private Adapter adapter;
-
+	
 	private static final String GESTOR = "gestor";
 	private static final String PROCESSO = "processo";
 	private static final String MUNICIPIO = "municipio";
@@ -43,34 +38,32 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	private static final String CODIG0_GESTOR = "codigo_gestor";
 	private static final String CODIGO_MUNICIPIO = "codigo_municipio";
 	private static final String CONTENT = "_content";
-	private static final String RSP = "rsp";
-
+	private static final String CONTENT2 = "rsp";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-				
-		listView = (ListView) findViewById(R.id.lista);
-		adapter = new Adapter(this, politicos);
-		listView.setAdapter(adapter);
 		
-		dialog = new ProgressDialog(this);
+		setContentView(R.layout.activity_testa_volley);
+		/*dialog = new ProgressDialog(this);
 		dialog.setMessage("Carregando");
-		dialog.show();
-		
+		dialog.show();*/
+	}
+	
+	public void methodJsonObject(View view){
 		JsonObjectRequest request = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
 
 			@Override
 			public void onResponse(JSONObject response) {
-				Log.d(TAG, "onResponse: "+response.toString());
+				Log.d(TAG, response.toString());
 				hideDialog();
 				
-				for(int i = 0; i < response.length(); i++ ){
+				for(int i = 0; i < response.length(); i++ ){	
 					try {
+						//JSONObject obj = response.getJSONObject(i);
 						Politico politico = new Politico();
-						JSONObject object = response.getJSONObject(RSP);
-						JSONArray array = object.getJSONArray(CONTENT);
 						
+						JSONArray array = response.getJSONArray(CONTENT2);
 							for(int j=0; j < array.length();j++){
 								JSONObject politicoJSON = new JSONObject(array.getString(j));
 								politico.setGestor(politicoJSON.getString(GESTOR));
@@ -85,17 +78,19 @@ public class MainActivity extends Activity implements OnItemClickListener {
 								politico.setCodigo_gestor(codigo_gestor);
 								double codigo_municipio = Double.parseDouble(politicoJSON.getString(CODIGO_MUNICIPIO));
 								politico.setCodigo_municipio(codigo_municipio);
-								politicos.add(politico);
-				
+
 							}
+							
+						politicos.add(politico);
+				        //Toast.makeText(MainActivity.this, politicos.get(0).getGestor(), Toast.LENGTH_LONG).show();
+						
+						
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 					
 				}
-				listView.setAdapter(adapter);
-		        listView.setCacheColorHint(Color.TRANSPARENT);
-		        adapter.notifyDataSetChanged();
+				
 			}
 		}, new Response.ErrorListener() {
 
@@ -107,19 +102,12 @@ public class MainActivity extends Activity implements OnItemClickListener {
 			}
 		});
 		AppController.getInstance().addToRequestQueue(request);
-				
 	}
+	
 	private void hideDialog() {
 		if (dialog != null) {
 			dialog.dismiss();
 			dialog = null;
 		}
 	}
-
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
